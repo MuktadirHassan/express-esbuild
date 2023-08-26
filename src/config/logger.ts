161749b9d6746e2b5a543cc8e3cafb77c1dba2e9
MuktadirHassan <tamimmuktadir@gmail.com>
2023-08-26 11:35:09 +0600

    (core): opts added for logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,6 +1,25 @@
 import pino from "pino";
 import { z } from "zod";
+import { isDev } from "./constants";
 
+// const opts: pino.LoggerOptions = {
+//   level: isDev ? "trace" : "info",
+
+//   transport: {
+//     targets: [
+//       {
+//         level: "trace",
+//         target: "pino-pretty",
+//         options: {},
+//       },
+//     ],
+//   },
+//   formatters: {
+//     bindings(bindings) {
+//       return {};
+//     },
+//   },
+// };
 export const logger = pino({});
 export const levelSchema = z.enum([
   "trace",
